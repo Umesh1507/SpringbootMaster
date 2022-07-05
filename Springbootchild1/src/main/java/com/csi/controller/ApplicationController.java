26cdecb4baf30489588681da3146de292c0e6d37
Umesh <you@example.com>
2022-07-05 09:22:20 +0530

    Service updated

---
+++ Springbootchild1/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,26 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/V1")
+public class ApplicationController {
+
+	@GetMapping("/Name")
+	public String getName() {
+		return "Umesh Navanath Thombre";
+	}
+
+	@GetMapping("/Education")
+	public String getEducation() {
+		return "Bachlore in engineering";
+	}
+
+	@GetMapping("/Service")
+	public String getAddress() {
+		return "Software Developement";
+	}
+
+}
